cfbab028f84ce1c2168db35f184ddaa3f3743e70
mehmetkorkutt <90136075+mehmetkorkutt@users.noreply.github.com>
2022-05-17 23:19:15 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,59 +1,156 @@
-#include<stdio.h>
-#include<string.h>
-
-int main() {
-	
-	int islem;
-	printf(" 1.Dosya olustur\n 2.Ogrenci \n 3.Yeni Kayit Ekle\n 4.Cikis\n");
-	printf("Yapmak istediginiz islem numarasini giriniz.\n");
-	scanf("%d",&islem);
-	
-	char adsoyad[50];
-	char bolum[50];
-	int num;
-	
-	
-	switch(islem){
-		
-		case 1: {
-			FILE *filep;
-			filep= fopen("ogrenci_bilgi.txt","w");
-			printf("Dosya olusturuldu.\n");
-			break;
-			
-		}
-		
-		case 2: {
-			FILE *filep;
-			filep= fopen("ogrenci_bilgi.txt","a+");
-			fscanf(filep,"%s %s %d",adsoyad,bolum,&num);
-			printf(" %s\n %s\n %d\n",adsoyad,bolum,num);
-			break;
-		}
-		
-		case 3: {
-			FILE *filep; 
-			filep=fopen("ogrenci_bilgi.txt","a+");
-			printf("Bilgileri giriniz:\n");
-			fflush(stdin);
-			fgets(adsoyad,128,stdin);
-			
-			fgets(bolum,128,stdin);
-			scanf("%d",&num);
-			
-			fputs(adsoyad,filep);
-			fputs(bolum,filep);
-			fprintf(filep,"%d",num);
-			
-			
-			printf("Kayit eklendi\n");
-			
-			break;
-			
-		}
-		
-		
-	}
-	
-	return 0;
-}
+#include<stdio.h> 
+ #include<stdlib.h> 
+  
+ struct ogrenci{ 
+          
+         int no; 
+         char ad[40]; 
+         char bolum[40]; 
+          
+ }; 
+  
+ typedef struct ogrenci ogrenci; 
+  
+ void VeriAlma(FILE *myptr){ 
+          
+         char bugfix[2]; 
+         ogrenci temp; 
+          
+         printf("\n----------Veri Alma-----------------\n EOF girilene kadar veri alinacaktir.\n"); 
+          
+                 printf("\n"); 
+                 printf("[Ogrenci no] [isim] [bolum] giriniz : "); 
+                 scanf("%d %s %s",&temp.no,temp.ad,temp.bolum); 
+          
+         while(!feof(stdin)){ 
+                  
+                 fprintf(myptr,"%s %d %s \n",temp.ad,temp.no,temp.bolum); 
+                  
+                 printf("[Ogrenci no] [isim] [bolum] giriniz : "); 
+                 scanf("%d %s %s",&temp.no,temp.ad,temp.bolum); 
+                                  
+         } 
+          
+         printf("\n------Veri ekleme islemi tamamlandi------------\n\n"); 
+          
+ } 
+  
+ void VeriYazma(FILE *myptr){ 
+          
+         ogrenci temp; 
+          
+         fscanf(myptr,"%s %d %s",temp.ad,&temp.no,temp.bolum); 
+          
+         printf("\n-----Ogrenci Listesi-----------\n"); 
+          
+         while(!feof(myptr)){ 
+                  
+                 printf("%d %s %s \n",temp.no,temp.ad,temp.bolum);                 
+                 fscanf(myptr,"%s %d %s",temp.ad,&temp.no,temp.bolum); 
+                  
+         } 
+          
+         printf("-------Listeleme islemi tamamlandi.-----\n\n"); 
+                  
+ } 
+  
+ int IsimArama(FILE *myptr, char name[]){ 
+          
+         ogrenci temp; 
+          
+         fscanf(myptr,"%s %d %s",temp.ad,&temp.no,temp.bolum); 
+                  
+         while(!feof(myptr)){ 
+                  
+                 if(strcmp(temp.ad,name)==0){ 
+                          
+                         printf("\naradiginiz ogrenci ; \n"); 
+                         printf("%d %s %s \n-------------\n",temp.no,temp.ad,temp.bolum); 
+                         return 1; 
+                          
+                 }else fscanf(myptr,"%s %d %s",temp.ad,&temp.no,temp.bolum); 
+                  
+         } 
+          
+         printf("\nAradiginiz ogrenci bulunamadi.\n"); 
+         return 0; 
+          
+ }         
+  
+ int main(){ 
+          
+         FILE *myptr; 
+          
+         int kontrol; 
+         char name[40]; 
+         char bugfix[2]; 
+          
+         while(1){ 
+                  
+                 printf("----------------MENU---------------\n1-Dosya olusturma\n2-Listeleme\n3-Yeni Kayit Ekleme\n4-Isme gore arama\n5-Cikis\nBir veri girisi gerceklestiriniz [1-5] : "); 
+                 scanf("%d",&kontrol); 
+                 if(kontrol==5){ 
+                          
+                         printf("\nCikis gerceklestiriliyor..."); 
+                         break; 
+                          
+                 } 
+                  
+                 switch(kontrol){ 
+                  
+                         case 1: 
+                          
+                                 myptr=fopen("OgrenciVerileri.txt","w");                         
+                                 if(myptr==NULL) printf("\nDosya acilamadi.\n"); 
+                                 VeriAlma(myptr); 
+                                 fclose(myptr); 
+                          
+                         break; 
+                  
+                         case 2: 
+                                  
+                                 myptr=fopen("OgrenciVerileri.txt","r"); 
+                                 if(myptr==NULL) printf("\nDosya acilamadi.\n"); 
+                                 VeriYazma(myptr); 
+                                 fclose(myptr); 
+                          
+                         break; 
+                          
+                         case 3: 
+                                  
+                                 myptr=fopen("OgrenciVerileri.txt","a"); 
+                                 if(myptr==NULL) printf("\nDosya acilamadi.\n"); 
+                                 VeriAlma(myptr); 
+                                 fclose(myptr); 
+                                  
+                         break; 
+                          
+                         case 4: 
+                                  
+                                 myptr=fopen("OgrenciVerileri.txt","r"); 
+                                 if(myptr==NULL) printf("\nDosya acilamadi.\n"); 
+                                 printf("Aramak istediginiz ismi giriniz : "); 
+                                 gets(bugfix); 
+                                 gets(name); 
+                                 IsimArama(myptr,name); 
+                                 fclose(myptr); 
+                                  
+                         break; 
+                          
+                         case 5: 
+                                  
+                                 printf("\nCikis gerceklestiriliyor...\n"); 
+                                  
+                         break; 
+                          
+                         default: 
+                                  
+                                 printf("\nYanlis giris gerceklestirdiniz.\n"); 
+                                  
+                         break; 
+                  
+                 } 
+         } 
+                  
+ return 0; 
+ }
